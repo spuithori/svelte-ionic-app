b67f0cdff2b0d13e356c64bc8c903c96eaaa07b5
Tommertom <Tom.Gruintjes@gmail.com>
2022-10-26 20:29:02 +0200

    SvelteKit Ionic 6 has landed!

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
-	},
+	}, 
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
