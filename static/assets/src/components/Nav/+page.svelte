0e37560ac76a6a1755ef46ff3267fd22939ce993
Tommertom <Tom.Gruintjes@gmail.com>
2023-01-13 18:22:00 +0100

    Ionic Nav update

--- static/assets/src/components/Nav/+page.svelte
+++ static/assets/src/components/Nav/+page.svelte
@@ -1,48 +1,10 @@
 <script lang="ts">
+	import { IonNav } from 'ionic-svelte';
 	import NavHome from '$lib/components/NavHome.svelte';
-	import { IonPage } from 'ionic-svelte';
-	import { onMount, SvelteComponent } from 'svelte';
-
-	// moving this to new IonNav element
-	let ionNav: HTMLIonNavElement;
-
-	const createHTMLCompFromSvelte = (
-		component: new (...args: any) => SvelteComponent,
-		componentProps: {}
-	) => {
-		const divWrapper = document.createElement('div');
-		const contentID = 'id' + Date.now();
-		divWrapper.id = contentID;
-
-		let navContent = document.createElement('div');
-
-		divWrapper.appendChild(navContent);
-		document.body.appendChild(divWrapper);
-
-		const props = {
-			...componentProps,
-			ionNav
-		};
-
-		const svelteComponent = new component({
-			target: navContent,
-			props
-		});
-
-		return divWrapper;
-	};
-
-	let root: HTMLElement;
-
-	onMount(() => {
-		root = createHTMLCompFromSvelte(NavHome, {});
-	});
 </script>
 
 <svelte:head>
 	<title>Ionic Companion - Nav</title>
 </svelte:head>
 
-<IonPage>
-	<ion-nav bind:this={ionNav} {root} />
-</IonPage>
+<IonNav root={NavHome} />
