c3be12c1a4bba1719b80bb4170facdd76ad8d1fe
Tommertom <Tom.Gruintjes@gmail.com>
2023-05-03 22:45:41 +0200

    more work on ionic7

--- demo-app/src/routes/components/tabs/+layout.svelte
+++ demo-app/src/routes/components/tabs/+layout.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
-	import IonTabs from './IonTabs.svelte';
-	import { videocam, pin } from 'ionicons/icons';
+	import IonTabs from 'ionic-svelte/components/IonTabs.svelte';
+
+	import { videocam, pin, musicalNote } from 'ionicons/icons';
 
 	const myTabs = [
 		{
 			label: 'Explain',
 			icon: pin,
-			tab: 'test1'
+			tab: 'explain'
 		},
-		{ label: 'Controllers', icon: videocam, tab: 'test2' }
+		{ label: 'Controllers', icon: videocam, tab: 'controllers' },
+		{ label: 'Blabla', icon: musicalNote, tab: 'blabla' }
 	];
 
-	const logStuff = (event: { detail: { tab: any }; type: any }) => {
-		console.log('>', event?.detail?.tab, event?.type);
-	};
+	const logStuff = () => {};
 </script>
 
-<IonTabs
-	ionTabsWillChange={logStuff}
-	ionNavWillLoad={logStuff}
-	ionTabsDidChange={logStuff}
-	slot="bottom"
-	tabs={myTabs}>
+<IonTabs slot="bottom" tabs={myTabs} ionTabsWillChange={logStuff} ionTabsDidChange={logStuff}>
 	<slot />
 </IonTabs>
